0913f918074155024ba1e28d9f3e8d7b841d397a
Navneet-Singh57 <navneet.vips57@gmail.com>
2024-10-26 15:15:56 +0530

    Customer Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -10,6 +10,7 @@ from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from vendor.models import Vendor
 from django.template.defaultfilters import slugify
+from orders.models import Order
 
 
 
@@ -163,7 +164,14 @@ def myAccount(request):
 @login_required(login_url='login')
 @user_passes_test(check_role_customer)
 def customerDashboard(request):
-    return render(request,'accounts/customerDashboard.html')
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')
+    recent_orders = orders[:5]
+    context = {
+        'orders': orders,
+        'orders_count': orders.count(),
+        'recent_orders':recent_orders
+    }
+    return render(request,'accounts/customerDashboard.html',context)
 
 @login_required(login_url='login')
 @user_passes_test(check_role_vendor)
